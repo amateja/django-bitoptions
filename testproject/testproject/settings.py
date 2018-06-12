3d7ceaad012827ada95841b7b685cbd8d8cd54c7
Andrzej Mateja <mateja.and@gmail.com>
2018-06-12 13:53:58 +0200

    Settings adjusted for Django2. Keeping MIDDLEWARE_CLASSES label for backward compatibility. Removed from Django project SessionAuthenticationMiddleware class removed from settings.

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -19,16 +19,15 @@ INSTALLED_APPS = (
     'testapp'
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-)
+]
 
 ROOT_URLCONF = 'testproject.urls'
 
