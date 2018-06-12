d6790860f72004254cf033b64fbdc263e61a53d5
Andrzej Mateja <mateja.and@gmail.com>
2018-06-12 13:37:49 +0200

    [Flake8] Double quotes replaced with single quotes to maintain consistency.

--- testproject/manage.py
+++ testproject/manage.py
@@ -4,8 +4,8 @@ import sys
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings")
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproject.settings')
 
     from django.core.management import execute_from_command_line
 
